6f0c216cad77fb0ece617832c7e2bb27f6c84bb0
dinhnguyenminhhoang <dinhnguyenminhhoang28@gmail.com>
2024-12-30 01:39:51 +0700

    [feat]:settup client && authu page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,35 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+import React from "react";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Login from "./Pages/Auth/Login/Login";
+import Register from "./Pages/Auth/Register/Register";
+import DefautLayout from "./Layout/DefautLayout/DefautLayout";
+import Home from "./Pages/Home/Home";
+import AdminLayout from "./Layout/AdminLayout/AdminLayout";
+const App = () => {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/">
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route element={<DefautLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+        <Route element={<AdminLayout />}></Route>
+        {/* <Route path="*" element={<PageNotFound />} />s */}
+      </Route>
+    )
+  );
+
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
+};
+
+export default App;
